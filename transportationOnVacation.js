21a0d4618692fe089247e681e5cd4bf8c96da671
kkotupalli <kartheek_kotupalli@epam.com>
2017-04-26 19:00:53 +0530

    updated with code review comments

--- transportationOnVacation.js
+++ transportationOnVacation.js
@@ -1,14 +1,11 @@
 function rentalCarCost(d) {
- var discount;
-	var rentForOneDay = 40;
-	if(d >= 7){
-		discount = 50;
-	}
-	else if (d >= 3){
-		discount = 20;
-	}
-	else{
-		discount = 0;
-	}
-	return ((d * rentForOneDay) - discount);
+  var discount;
+  var rentForOneDay = 40;
+  if(d >= 7)
+     discount = 50;
+  else if (d >= 3)
+     discount = 20;
+  else
+     discount = 0;
+  return ((d * rentForOneDay) - discount);
 }
