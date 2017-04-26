1540f271a179435b625cefa9985c5743847e41f9
kkotupalli <kartheek_kotupalli@epam.com>
2017-04-26 18:50:13 +0530

    updated with code review comments

--- Middlecharacter.js
+++ Middlecharacter.js
@@ -1,12 +1,9 @@
-function getMiddle(s)
-{
-  var wordLength = s.length;
-	var middleChar;
-	if(wordLength % 2 == 0){
-		middleChar = s.substr((wordLength / 2 - 1),2);
-	}
-	else{
-		middleChar = s.substr((wordLength / 2 ),1);
-	}
-	return middleChar;
+function getMiddle(s) {
+   var wordLength = s.length;
+   var middleChar;
+   if (wordLength % 2 == 0)
+       middleChar = s.substr(Math.floor((wordLength / 2 - 1)), 2);
+   else
+       middleChar = s.substr(Math.floor((wordLength / 2)), 1);
+   return middleChar;
 }
