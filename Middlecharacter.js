b2467017bdffc423d74e9babcb454cbd9fec344c
kkotupalli <kartheek_kotupalli@epam.com>
2017-05-05 13:29:40 +0530

    Delete Middlecharacter.js

--- Middlecharacter.js
+++
@@ -1,9 +0,0 @@
-function getMiddle(s) {
-   var wordLength = s.length;
-   var middleChar;
-   if (wordLength % 2)
-      middleChar = s.substr(Math.floor((wordLength / 2)), 1); 
-   else
-      middleChar = s.substr(Math.floor((wordLength / 2 - 1)), 2); 
-   return middleChar;
-}
