446e6acd6bc76e9a405a0269bb82544e8678b20c
kkotupalli <kartheek_kotupalli@epam.com>
2017-05-05 17:49:57 +0530

    new with apply initial check-in

--- HW-3/ApplyWithNew.js
+++ HW-3/ApplyWithNew.js
@@ -1,1 +1,10 @@
-
+function factory(){
+    return {
+        createGreeting() {
+           return construct(Greeting, arguments);
+        }
+    }
+}
+function construct(Class){
+    return new(Class.bind.apply(Class, arguments))()
+}
