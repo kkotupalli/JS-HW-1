d3db3aee9c94c2a3c6b67904789e06b510f56d44
kkotupalli <kartheek_kotupalli@epam.com>
2017-05-05 13:30:03 +0530

    Delete WordCount.js

--- WordCount.js
+++
@@ -1,9 +0,0 @@
-function countWords(str) {
-  var inputString = str.trim(); 
-  var count = 0;
-  if(inputString=='')
-      count = 0;
-  else
-      count = inputString.split(/\s+/).length; 
-  return count;
-}
