9c0b1432523aad0c6aa97591741469d915853486
kkotupalli <kartheek_kotupalli@epam.com>
2017-05-12 14:27:15 +0530

    Angular Js HW-4/1

---
+++ HW-4/Controllers/CatAndClick.js
@@ -0,0 +1,13 @@
+﻿(function (module) {
+    module.controller("CatAndClickController", function ($scope) {
+        $scope.numberOfClicks = 0;
+        $scope.ModifyNumberOfClicks = function () {
+            $scope.numberOfClicks++;
+        }
+        $scope.ResetClicks = function () {
+            $scope.numberOfClicks = 0;
+        }
+    });
+
+})(angular.module("CatAndClick"));
+
