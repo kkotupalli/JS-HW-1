1fbbe32e8aa7c6b45465f46956283b3c70179052
kkotupalli <kartheek_kotupalli@epam.com>
2017-05-12 16:35:38 +0530

    Initial Draft

---
+++ HW-4 1.2/Controllers/CatAndClick.js
@@ -0,0 +1,16 @@
+﻿(function (module) {
+    module.controller("CatAndClickController", CatAndClickController);
+})(angular.module("CatAndClick"));
+function CatAndClickController() {
+    debugger;
+    var _context = this;
+    _context._catsList = [{ name: 'Jerry', ClickCount: 0 }, { name: 'Mickey', ClickCount: 0 }];
+    _context.ClickTheCat = function (name) {
+        debugger;
+        for (var catcount = 0; catcount < _context._catsList.length; catcount++) {
+            if (_context._catsList[catcount].name == name) {
+                _context._catsList[catcount].ClickCount++;
+            }
+        }
+    }
+}
---
+++ HW-4 1.2/Scripts/app.js
@@ -0,0 +1,5 @@
+﻿(function (angular) {
+
+    angular.module("CatAndClick", []);
+
+})(angular);
